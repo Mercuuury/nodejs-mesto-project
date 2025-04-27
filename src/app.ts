a7432cee4e6b4ddb7b555f06a2b6f6a9ed519633
Mercuuury <kovshiksa@gmail.com>
2025-04-27 14:47:45 +0300

    feat: :loud_sound: add request & error logging with `winston`

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
-import { auth, errorHandler, routeInvalidator } from './middlewares';
+import {
+  auth, errorHandler, errorLogger, requestLogger, routeInvalidator,
+} from './middlewares';
 import { cardsRouter, usersRouter } from './routes';
 import { createUser, login } from './controllers/users';
 
@@ -14,6 +16,8 @@ app.use(express.json());
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
+app.use(requestLogger);
+
 app.post('/signin', login);
 app.post('/signup', createUser);
 
@@ -23,6 +27,8 @@ app.use('/users', usersRouter);
 app.use('/cards', cardsRouter);
 
 app.use(routeInvalidator);
+
+app.use(errorLogger);
 
 app.use(errorHandler);
 
--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,3 +1,4 @@
 export { default as auth } from './auth';
 export { default as errorHandler } from './errorHandler';
 export { default as routeInvalidator } from './routeInvalidator';
+export { requestLogger, errorLogger } from './logger';
